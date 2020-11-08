428d8806eb46a918d26d9ab058389e1c0a16ab6a
Sinht <Sinht@DESKTOP-75M15OO.Speedport_W_724V_Typ_A_05011603_06_003>
2020-11-08 12:59:33 +0100

    Ui.created

--- randomItemgenerator/src/randomItemgenerator/mainProgram.java
+++ randomItemgenerator/src/randomItemgenerator/mainProgram.java
@@ -1,9 +1,20 @@
 package randomItemgenerator;
 
+import java.awt.EventQueue;
+
 public class mainProgram {
 
 	public static void main(String[] args) {
-	
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					generatorUi window = new generatorUi();
+					window.getFrame().setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
 		
 		
