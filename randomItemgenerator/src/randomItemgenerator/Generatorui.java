31fe9516fde2a8a110f3f21580e3aea277479d5d
SinhtUp <Hamit.Alkin@gmail.com>
2021-04-07 19:43:01 +0200

    Ui angepasst

--- randomItemgenerator/src/randomItemgenerator/Generatorui.java
+++ randomItemgenerator/src/randomItemgenerator/Generatorui.java
@@ -12,10 +12,12 @@ import java.awt.Color;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import java.awt.Font;
+import java.awt.Dimension;
+import java.util.Locale;
 
 public class Generatorui {
 	
-	private JFrame frame;
+	private JFrame frmRandomItemGenerator;
 	private JTextField textField;
 	
 	
@@ -27,95 +29,112 @@ public class Generatorui {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setFont(UIManager.getFont("ColorChooser.font"));
-		frame.getContentPane().setBackground(UIManager.getColor("Button.background"));
-		frame.setBackground(Color.LIGHT_GRAY);
-		getFrame().setBounds(100, 100, 698, 411);
+		frmRandomItemGenerator = new JFrame();
+		frmRandomItemGenerator.setTitle("Random Item Generator (Beta)");
+		frmRandomItemGenerator.setResizable(false);
+		frmRandomItemGenerator.getContentPane().setLocale(Locale.GERMANY);
+		frmRandomItemGenerator.getContentPane().setMaximumSize(new Dimension(720, 430));
+		frmRandomItemGenerator.getContentPane().setFont(UIManager.getFont("ColorChooser.font"));
+		frmRandomItemGenerator.getContentPane().setBackground(Color.GRAY);
+		frmRandomItemGenerator.setBackground(Color.LIGHT_GRAY);
+		getFrame().setBounds(100, 100, 720, 430);
 		getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmRandomItemGenerator.getContentPane().setLayout(null);
 		
 		JRadioButton rdbtnShortsword = new JRadioButton("Kurzschwert");
+		rdbtnShortsword.setBorder(null);
+		rdbtnShortsword.setForeground(Color.BLACK);
 		rdbtnShortsword.setBounds(8, 8, 120, 15);
-		frame.getContentPane().add(rdbtnShortsword);
+		rdbtnShortsword.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnShortsword);
 		
 		JRadioButton rdbtnLongSword = new JRadioButton("Langschwert");
 		rdbtnLongSword.setBounds(132, 8, 120, 15);
-		frame.getContentPane().add(rdbtnLongSword);
+		rdbtnLongSword.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnLongSword);
 		
 		JRadioButton rdbtnDagger = new JRadioButton("Dolch");
 		rdbtnDagger.setBounds(256, 8, 71, 15);
-		frame.getContentPane().add(rdbtnDagger);
+		rdbtnDagger.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnDagger);
 		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Stab");
 		rdbtnNewRadioButton_1.setBounds(331, 8, 71, 15);
-		frame.getContentPane().add(rdbtnNewRadioButton_1);
+		rdbtnNewRadioButton_1.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnNewRadioButton_1);
 		
 		JRadioButton rdbtnBogen = new JRadioButton("Bogen");
 		rdbtnBogen.setBounds(406, 8, 78, 15);
-		frame.getContentPane().add(rdbtnBogen);
+		rdbtnBogen.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnBogen);
 		
 		JTextPane txtpnTest = new JTextPane();
 		txtpnTest.setEditable(false);
 		txtpnTest.setBounds(12, 88, 468, 279);
-		frame.getContentPane().add(txtpnTest);
+		frmRandomItemGenerator.getContentPane().add(txtpnTest);
 		
 		textField = new JTextField();
 		textField.setBounds(8, 53, 71, 23);
-		frame.getContentPane().add(textField);
+		frmRandomItemGenerator.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblLevel = new JLabel("Level");
 		lblLevel.setBounds(83, 57, 70, 15);
-		frame.getContentPane().add(lblLevel);
+		frmRandomItemGenerator.getContentPane().add(lblLevel);
 		
 		JButton btnNewButton_1 = new JButton("Generieren");
 		btnNewButton_1.setBounds(508, 308, 174, 59);
-		frame.getContentPane().add(btnNewButton_1);
+		frmRandomItemGenerator.getContentPane().add(btnNewButton_1);
 		
 		JSeparator separator = new JSeparator();
 		separator.setOrientation(SwingConstants.VERTICAL);
 		separator.setForeground(Color.WHITE);
 		separator.setBackground(Color.DARK_GRAY);
 		separator.setBounds(492, 8, 63, 371);
-		frame.getContentPane().add(separator);
+		frmRandomItemGenerator.getContentPane().add(separator);
 		
 		JRadioButton rdbtnFeuer = new JRadioButton("Feuer");
 		rdbtnFeuer.setBounds(508, 34, 149, 23);
-		frame.getContentPane().add(rdbtnFeuer);
+		rdbtnFeuer.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnFeuer);
 		
 		JRadioButton rdbtnWind = new JRadioButton("Wind");
 		rdbtnWind.setBounds(508, 61, 149, 23);
-		frame.getContentPane().add(rdbtnWind);
+		rdbtnWind.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnWind);
 		
 		JRadioButton rdbtnEis = new JRadioButton("Eis");
 		rdbtnEis.setBounds(508, 86, 149, 23);
-		frame.getContentPane().add(rdbtnEis);
+		rdbtnEis.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnEis);
 		
 		JRadioButton rdbtnBlitz = new JRadioButton("Blitz");
 		rdbtnBlitz.setBounds(508, 113, 149, 23);
-		frame.getContentPane().add(rdbtnBlitz);
+		rdbtnBlitz.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnBlitz);
 		
 		JRadioButton rdbtnLicht = new JRadioButton("Licht");
 		rdbtnLicht.setBounds(508, 140, 149, 23);
-		frame.getContentPane().add(rdbtnLicht);
+		rdbtnLicht.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnLicht);
 		
 		JRadioButton rdbtnFinsterniss = new JRadioButton("Finsterniss");
 		rdbtnFinsterniss.setBounds(508, 167, 149, 23);
-		frame.getContentPane().add(rdbtnFinsterniss);
+		rdbtnFinsterniss.setBackground(null);
+		frmRandomItemGenerator.getContentPane().add(rdbtnFinsterniss);
 		
 		JLabel lblEffekt = new JLabel("Effekte");
 		lblEffekt.setFont(new Font("Dialog", Font.BOLD, 15));
 		lblEffekt.setBounds(508, 12, 120, 15);
-		frame.getContentPane().add(lblEffekt);
+		frmRandomItemGenerator.getContentPane().add(lblEffekt);
 		
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setForeground(UIManager.getColor("Button.disabledText"));
 		separator_1.setBounds(0, 29, 694, 7);
-		frame.getContentPane().add(separator_1);
+		frmRandomItemGenerator.getContentPane().add(separator_1);
 	}
 
 	public JFrame getFrame() {
-		return frame;
+		return frmRandomItemGenerator;
 	}
 }
